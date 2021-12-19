45611708f43c8f8ac4b913eaa6a93521bdd3e541
Sridhar <sridharthechosenone@gmail.com>
2021-12-19 15:02:26 +0530

    Update profile pic

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,6 +1,6 @@
 const homeScreenConfig = {
   profilePicUrl:
-    'https://media-exp1.licdn.com/dms/image/C5603AQGVIiwttuV_Qg/profile-displayphoto-shrink_800_800/0/1626363420185?e=1637193600&v=beta&t=6E0TV2RNA0r6gJjcDo4rU2i3EVhboDBpnz2RVqDrl_Y',
+    'https://firebasestorage.googleapis.com/v0/b/portfolio-930c6.appspot.com/o/public%2Fportfolio%2Fprofile_pic.jpeg?alt=media&token=30f83982-a90f-4e77-8a0e-26a4b80d7d86',
   profileName: 'Sridhar Subramani',
   profileShortDesc: `Self-taught software developer with great problem-solving skills,
   having 5+ years of craftsmanship in developing awesome android
