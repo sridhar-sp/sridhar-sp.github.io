3871fcf40db04f6e9a4a7187e3ffdc6cddc2ff1d
sridhar-sp <sridharthechosenone@gmail.com>
2022-09-08 21:23:25 +0530

    Update font style

--- src/theme/Colors.js
+++ src/theme/Colors.js
@@ -1,21 +1,21 @@
 const LightColors = {
-  navbar: '#FFFFFF',
-  background: '#F6F6F6',
-  onBackground: '#000000',
-  primaryText: '#000000',
-  secondaryText: '#767676',
-  surface: '#FFFFFF',
-  onSurface: '#000000',
-}
+  navbar: "#FFFFFF",
+  background: "#F0F0F0",
+  onBackground: "#000000",
+  primaryText: "#000000",
+  secondaryText: "#767676",
+  surface: "#FFFFFF",
+  onSurface: "#000000",
+};
 
 const DarkColors = {
-  navbar: '#050505',
-  background: '#121212',
-  onBackground: '#FFFFFF',
-  primaryText: '#FFFFFF',
-  secondaryText: '#bbbbbb',
-  surface: '#121212',
-  onSurface: '#DDFFFFFF',
-}
+  navbar: "#121212",
+  background: "#252525",
+  onBackground: "#FFFFFF",
+  primaryText: "#FFFFFF",
+  secondaryText: "#bbbbbb",
+  surface: "#121212",
+  onSurface: "#DDFFFFFF",
+};
 
-export { LightColors, DarkColors }
+export { LightColors, DarkColors };
